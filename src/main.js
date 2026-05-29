92e5fc14f0da8ae14e9c342d78108f8062f747a1
Eugene Hauptmann <eugene@reactivelions.com>
2026-05-29 09:18:21 -0400

    added analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import { inject } from '@vercel/analytics';
 import { mount } from 'svelte';
 import './app.css';
 import { initTheme } from './lib/theme.js';
 import App from './App.svelte';
+
+if (import.meta.env.PROD) {
+  inject();
+}
 
 initTheme();
 
